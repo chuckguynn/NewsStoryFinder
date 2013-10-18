4c37752701ab3a1476d62a015903839fe940a7c4
chuckguynn <chuckguynn@me.com>
2013-10-18 10:59:41 -0500

    set up paperclip for s3 storage

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,6 +5,13 @@ class Story < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :image
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
+  has_attached_file :image,
+  styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  },
+  :storage => :s3,
+  :s3_credentials => "#{Rails.root}/config/s3.yml"
 end
